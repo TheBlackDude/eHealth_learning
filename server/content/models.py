d44caf3ed357773df8dad411606d6b76f6c05f98
Osman Jalloh <omudu45@hotmail.com>
2016-08-30 22:14:55 +0000

    start the content app and model for the Hosts.

---
+++ server/content/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Host(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=100)
+    bio = models.TextField(default='')
+
+    class Meta:
+        verbose_name = 'Host'
+        verbose_name_plural = 'Hosts'
+
+    def __str__(self):
+        return self.name
