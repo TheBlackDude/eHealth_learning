fa963e77e82d28aabfb5cdef328619e6a3d0098b
Osman Jalloh <omudu45@hotmail.com>
2016-09-06 10:46:18 +0000

    add the Academy Info Page.

---
+++ client/controllers/InfoController.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+
+    angular.module('eHealth.controllers')
+    .controller('InfoCtrl', ['$scope', function($scope) {
+        var vm = this;
+        
+        vm.greet = 'this is the Academy info page'
+
+    }]);
+
+})();
