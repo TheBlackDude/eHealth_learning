4dc0911dd4289d975f9bb07ac3aa665a2afba28c
Osman Jalloh <omudu45@hotmail.com>
2016-09-10 14:12:09 +0000

    add the Snackbar service.

---
+++ client/services/SnackbarService.js
@@ -0,0 +1,30 @@
+(function($, _) {
+    'use strict';
+
+    angular.module('eHealth.utils')
+    .factory('Snackbar', [function() {
+        var Snackbar = {
+            error: error,
+            show: show
+        }
+
+        function _snackbar(content, options) {
+            options = _.extend({timeout: 3000}, options);
+            options.content = content; 
+
+            $.snackbar(options);
+        }
+
+        function error(content, options) {
+            _snackbar('Error:' + content, options)
+        }
+
+        function show(content, options) {
+            _snackbar(content, options);
+        }
+
+        return Snackbar;
+
+    }]);
+
+})($, _);
